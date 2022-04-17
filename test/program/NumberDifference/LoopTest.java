d76a872bcaf0d8b6bc7acf4ecb0229d44f1e1f94
friskaim7 <friska.intan2001@gmail.com>
2022-04-17 11:07:53 +0700

    add a test case (TC 6)

--- test/program/NumberDifference/LoopTest.java
+++ test/program/NumberDifference/LoopTest.java
@@ -1,5 +1,26 @@
 package program.NumberDifference;
 
-public class LoopTest extends NumberDifferenceTest{
-    
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class LoopTest extends NumberDifferenceTest {
+
+    @Test //TC6
+    public void testLoop9x() {
+        // (1) setup (arrange, build)
+        ArrayList<Integer> deretBil = new ArrayList<>(
+                Arrays.asList(1, 1, 1, 1, 1, 1, 1, 1, 1, 2));
+        int actual, expected = 1;
+
+        // (2) exercise (act, operate)
+        actual = sut.numberDiffirenceProcess(deretBil);
+
+        // (3) verify (assert, check)
+        assertEquals(expected, actual);
+    }
+
 }
