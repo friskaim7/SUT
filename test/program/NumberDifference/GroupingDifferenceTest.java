73807a1bd86cbef6c8bffb856c51de619a6fbab6
arahmi92@gmail.com <arahmi92@gmail.com>
2022-04-17 16:04:33 +0700

    add and update test cases

--- test/program/NumberDifference/GroupingDifferenceTest.java
+++ test/program/NumberDifference/GroupingDifferenceTest.java
@@ -6,6 +6,63 @@ import org.junit.Test;
 
 public class GroupingDifferenceTest extends NumberDifferenceTest {
 
+    @Test // TC1
+    public void testDifferenceResultInGroup1_01(){
+        ArrayList<Integer> deret = new ArrayList<>();
+        int n, diffActual, diffExpected;
+        String stringActual, stringExpected;
+        n=2;
+        deret.add(10);
+        deret.add(6);
+
+        diffExpected=4;
+        stringExpected="Difference : 4\nGroup 1, Small Difference";
+
+        diffActual=sut.numberDiffirenceProcess(deret);
+        stringActual=sut.groupingDifference(diffActual);
+
+        assertEquals(diffExpected, diffActual);
+        assertEquals(stringExpected, stringActual);
+    }
+
+    @Test // TC2
+    public void testDifferenceResultInGroup1_02(){
+        ArrayList<Integer> deret = new ArrayList<>();
+        int n, diffActual, diffExpected;
+        String stringActual, stringExpected;
+        n=2;
+        deret.add(20);
+        deret.add(10);
+
+        diffExpected=10;
+        stringExpected="Difference : 10\nGroup 1, Small Difference";
+
+        diffActual=sut.numberDiffirenceProcess(deret);
+        stringActual=sut.groupingDifference(diffActual);
+
+        assertEquals(diffExpected, diffActual);
+        assertEquals(stringExpected, stringActual);
+    }
+
+    @Test // TC3
+    public void testDifferenceResultInGroup1_03(){
+        ArrayList<Integer> deret = new ArrayList<>();
+        int n, diffActual, diffExpected;
+        String stringActual, stringExpected;
+        n=2;
+        deret.add(10);
+        deret.add(9);
+
+        diffExpected=1;
+        stringExpected="Difference : 1\nGroup 1, Small Difference";
+
+        diffActual=sut.numberDiffirenceProcess(deret);
+        stringActual=sut.groupingDifference(diffActual);
+
+        assertEquals(diffExpected, diffActual);
+        assertEquals(stringExpected, stringActual);
+    }
+    
     @Test // TC8
     public void testDifferenceResultInGroup2_01() {
         // (1) setup (arrange, build)
--- test/program/NumberDifference/LoopTest.java
+++ test/program/NumberDifference/LoopTest.java
@@ -9,6 +9,52 @@ import org.junit.Test;
 
 public class LoopTest extends NumberDifferenceTest {
 
+    @Test //TC4
+    public void testLoop2x(){
+        ArrayList<Integer> deret = new ArrayList<>();
+        int n, diffActual, diffExpected;
+        String stringActual, stringExpected;
+        n=3;
+        deret.add(10);
+        deret.add(5);
+        deret.add(1);
+
+        diffExpected=9;
+        stringExpected="Difference : 9\nGroup 1, Small Difference";
+
+        diffActual=sut.numberDiffirenceProcess(deret);
+        stringActual=sut.groupingDifference(diffActual);
+
+        assertEquals(diffExpected, diffActual);
+        assertEquals(stringExpected, stringActual);
+    }
+
+    @Test //TC5
+    public void testLoop8x(){
+        ArrayList<Integer> deret = new ArrayList<>();
+        int n, diffActual, diffExpected;
+        String stringActual, stringExpected;
+        n=9;
+        deret.add(1);
+        deret.add(1);
+        deret.add(1);
+        deret.add(1);
+        deret.add(1);
+        deret.add(1);
+        deret.add(1);
+        deret.add(1);
+        deret.add(2);
+
+        diffExpected=1;
+        stringExpected="Difference : 1\nGroup 1, Small Difference";
+
+        diffActual=sut.numberDiffirenceProcess(deret);
+        stringActual=sut.groupingDifference(diffActual);
+
+        assertEquals(diffExpected, diffActual);
+        assertEquals(stringExpected, stringActual);
+    }
+    
     @Test //TC6
     public void testLoop9x() {
         // (1) setup (arrange, build)
