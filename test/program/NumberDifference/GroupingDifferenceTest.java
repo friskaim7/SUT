48a4e509fee39009c64584f1942622dfba904d74
friskaim7 <friska.intan2001@gmail.com>
2022-04-17 10:43:26 +0700

    Untrack files in .gitignore

--- test/program/NumberDifference/GroupingDifferenceTest.java
+++ test/program/NumberDifference/GroupingDifferenceTest.java
@@ -9,11 +9,8 @@ public class GroupingDifferenceTest extends NumberDifferenceTest{
     @Test
     public void testDifferenceResultInGroup1_02() {
         // (1) setup (arrange, build)
-        int diff;
-        String actual, expected;
-        
-        diff = 1;
-        expected = "Difference : 1\nGroup 1, Small Difference";
+        int diff = 1;
+        String actual, expected = "Difference : 1\nGroup 1, Small Difference";       
 
         // (2) exercise (act, operate)
         actual = sut.groupingDifference(diff);
